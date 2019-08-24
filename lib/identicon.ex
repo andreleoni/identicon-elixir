40d26dd4f3968f8ca7b47f8e1b2856a39e04277f
André Leoni <andreluizleoni@gmail.com>
2019-08-24 16:15:37 -0300

    Continuous the identicon build

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,51 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+  end
 
-  @doc """
-  Hello world.
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
 
-  ## Examples
+  end
 
-      iex> Identicon.hello()
-      :world
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid =
+      Enum.filter grid, fn({code, _index}) ->
+        rem(code, 2) == 0
+      end
 
-  """
-  def hello do
-    :world
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{ hex: hex }
   end
 end
